4272d422ce108003f3e3c2ec4c211f9f1b4d4f0a
Estlib <75094683+Estlib@users.noreply.github.com>
2023-01-26 18:13:21 +0200

    added data from database into model

--- backend-api/models/gacha.model.js
+++ backend-api/models/gacha.model.js
@@ -8,6 +8,50 @@ module.exports = (sequelize,Sequelize) => {
         name: {
             type: Sequelize.STRING,
             allowNull: false
+        },
+        item1Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item2Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item3Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item4Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item5Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item6Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item7Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item8Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item9Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        item10Rarity: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        gachaAmount: {
+            type: Sequelize.INTEGER,
+            allowNull: false
         }
     })
     return Gacha
