1789ac56ad047ef1649664980d2cd7e069e352c7
Estlib <75094683+Estlib@users.noreply.github.com>
2023-02-02 14:01:20 +0200

    find by primary key

--- backend-api/controllers/gachaController.js
+++ backend-api/controllers/gachaController.js
@@ -6,7 +6,7 @@ exports.getAll = async (req, res)=>{
     res.send(gachas)
 }
 exports.getById = async (req, res)=>{
-    const gachas = await Gachas.findAll({where: {id: req.params.id}})
+    const gachas = await Gachas.findByPk({where: {id: req.params.id}})
     res.send(gachas)
     //const gachas = await Gachas.findById({where:[id:]})
     //res.send(gachas)
