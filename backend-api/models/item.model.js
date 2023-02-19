77d9d22befc763e6dec70e0264711ac093e0653f
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-02-19 20:23:26 +0200

    item model manually added

---
+++ backend-api/models/item.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize,Sequelize) => {
+    const Item = sequelize.define('Item',{
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        ItemName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        ItemDescription: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        ItemImageLink: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        ItemSupply: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    })
+    return Item
+}
