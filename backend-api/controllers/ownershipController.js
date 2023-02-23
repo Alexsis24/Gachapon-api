a4e3001cf1c5265659824a5241ef42e2e1c7429d
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-02-23 12:44:51 +0200

    ownership model and controller

---
+++ backend-api/controllers/ownershipController.js
@@ -0,0 +1,85 @@
+const { request } = require("express")
+const {db} = require("../db")
+const Ownership = db.ownerships
+const User = db.users
+const Item = db.items
+
+exports.getAll = async (req, res)=>{
+    const ownerships = await Ownership.findAll({attributes:["id","OwnershipUserId","OwnershipItemId","Ownership"]})
+    res.send(ownerships)
+}
+exports.getById = async (req, res)=>{
+    const ownerships = await Ownership.findByPk(req.params.ownershipId)
+    if (ownerships === null) {
+        res.status(404).send({"error":"Ownership not found"})
+        return
+    }
+    res.send(ownerships)
+    //const gachas = await Gachas.findById({where:[id:]})
+    //res.send(gachas) delivers
+}
+exports.createNew = async (req,res) =>{
+    console.log("New Ownership: ",req.body)
+    let ownership
+    try {
+        ownership = await Ownerships.create(req.body,
+            {
+                logging: console.log,
+                include: [ item, user ]
+            })    
+        } catch (error) {
+            if (error instanceof db.Sequelize.ValidationError)
+            {
+                console.log(ownership)
+                res.status(400).send({"error":error.errors.map((x => x.message))})
+            } else if (error instanceof db.Sequelize.ForeignKeyConstraintError){
+                res.status(400).send({"error":`Table:${error.table} does not contain row with id:${error.value}`})
+            } else {
+                console.log("OwnershipsCreate: ",error);
+                res.status(500).send({"error":"Something went wrong on our side, a crack team of bughunting kittens has been dispatched :3"})
+            }
+            return
+    }
+    res.status(201).location(`${getBaseUrl(req)}/ownerships/${ownership.id}`).json(ownership)
+}
+
+exports.deleteById = async (req, res) => {
+    let result 
+    try {
+        result = await Ownerships.destroy({ where: { id: req.params.ownershipId } })
+    } catch (error) {
+        console.log("OwnershipsDelete: ",error)
+        res.status(500).send({"error":"Something went wrong on our side, a crack team of bughunting kittens has been dispatched :3"})
+        return
+    }    
+    if (result === null) {
+        res.status(404).send({"error":"Ownership not found"})
+        return
+    }
+    res.status(204).send()
+}
+
+exports.updateById = async (req, res) => {
+    let result 
+    delete req.body.id
+    console.log(req.body)
+    try {
+        result = await Ownerships.update(req.body,{ where: { id: req.params.ownershipId } })
+        console.log(result)
+    } catch (error) {
+        console.log("OwnershipsDelete: ",error)
+        res.status(500).send({"error":"Something went wrong on our side, a crack team of bughunting kittens has been dispatched :3 meow"})
+        return
+    }    
+    if (result===0) {
+        res.status(404).send({"error":"Ownership not found"})
+        return
+    }
+    const ownership = await Ownerships.findByPk(req.params.ownershipId)
+    console.log(ownership)
+    res.status(200).location(`${getBaseUrl(req)}/ownerships/${Ownership.id}`).json(ownership)
+}
+
+getBaseUrl = (request) => {
+    return ((request.connection &&request.connection.encrypted ? "https":"http") + `://${request.headers.host}`)
+}
