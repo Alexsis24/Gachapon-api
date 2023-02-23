202eeec33787638483d023b5981f58217b92fb3c
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-02-23 12:57:19 +0200

    Ownership Crud w/ merged Item and user

--- backend-api/routes/app_routes.js
+++ backend-api/routes/app_routes.js
@@ -6,25 +6,33 @@ module.exports = (app) => {
     app.route("/gachas")
         .get(gachaController.getAll)
         .post(gachaController.createNew) //Create
-        app.route("/gachas/:gachasId")
+    app.route("/gachas/:gachasId")
         .get(gachaController.getById) //Read
         .put(gachaController.updateById) //Update
         .delete(gachaController.deleteById) //Delete 
-        
+    ////////////////////////////////
     app.route("/users")
         .get(userController.getAll)
         .post(userController.createNew) //Create
-        app.route("/users/:userId")
+    app.route("/users/:userId")
         .get(userController.getById) //Read
         .put(userController.updateById) //Update
         .delete(userController.deleteById) //Delete 
-
+    ////////////////////////////////
     app.route("/items")
         .get(itemController.getAll)
         .post(itemController.createNew) //Create
-        app.route("/items/:itemId")
+    app.route("/items/:itemId")
         .get(itemController.getById) //Read
         .put(itemController.updateById) //Update
         .delete(itemController.deleteById) //Delete 
+    ////////////////////////////////
+    app.route("/ownerships")
+        .get(ownershipsController.getAll)
+        .post(ownershipsController.createNew) //Create
+    app.route("/ownerships/:ownershipId")
+        .get(ownershipsController.getById) //Read
+        .put(ownershipsController.updateById) //Update
+        .delete(ownershipsController.deleteById) //Delete
 
 }
