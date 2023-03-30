f19a3e3a2310357261581960a311f90f322ee8c3
Estlib <75094683+Estlib@users.noreply.github.com>
2023-03-30 11:37:09 +0300

    app vue new view for gachamachines, main page

---
+++ frontend-api-vue3/src/components/views/AllGachaponMachinesView.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+    <table-template 
+      caption="Kõik Gacha Masinad" 
+      :items="gachas" 
+      :showControls="true" 
+      @show="gachaDetailId = $event.id">
+    </table-template>
+    </div>
+  <gacha-details 
+    :gachaDetailId = "gachaDetailId"
+    @close="gachaDetailId = 0">
+  </gacha-details> 
+  </template>
+  
+  <script>
+  import TableTemplate from '../components/Table.vue';
+  import GachaDetails from '../components/GachaDetails.vue';
+  
+  export default {
+    components: {
+      GachaDetails,
+      TableTemplate,
+    }, 
+    data() {
+      return {
+        gachas: [],
+        gachaDetailId: 0,    
+      };
+    },
+    async created() {
+      this.gachas = await (await fetch("http://localhost:8090/gachas")).json()
+    },  
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    line-height: 1.5;
+  }
+  
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+  
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
+  
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+  
+    header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+  }
+  </style>
+  
